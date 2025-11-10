465fb496fcb28e71b9699124698f9692ac7f8508
rhog23 <raymonde.saragih@gmail.com>
2025-11-10 20:30:02 +0700

    check using C3Ghost

--- compile_model.py
+++ compile_model.py
@@ -2,4 +2,4 @@ from ultralytics.nn.tasks import ClassificationModel
 from torchsummary import summary
 
 model = ClassificationModel("yolo12n-ghost-cls.yaml")
-summary(model, (3, 224, 224)) 
+# summary(model, (3, 224, 224)) 
